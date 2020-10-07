f595dfb380e4decb895fdf11a57555d8d6083d85
Tage Johansson <40422978+tage64@users.noreply.github.com>
2020-10-07 11:06:44 +0200

    Add type annotation to buffer.lines and add buffer.current_file

--- buffer.py
+++ buffer.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+
 class Buffer:
     def __init__(self):
-        self.lines = []
+        self.lines: list[str] = []
         self.current_line_index: Optional[int] = None
+        self.current_file: Optional[str] = None
 
     def current_line(self) -> Optional[str]:
         if self.current_line_index is not None:

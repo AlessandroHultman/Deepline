faee7caef34e4750b34d8f915b8e501b9d7034d3
Alessandro Hultman <alessandrosisfontes@hotmail.com>
2020-10-07 11:56:39 +0200

    modified buffer_functions.py

--- buffer_functions.py
+++ buffer_functions.py
@@ -1,11 +1,18 @@
+import Buffer
 
+def insert(buffer, text):
+    lines = text.split("\n")
+    if buffer.current_line_index is None:
+        buffer.current_line_index = len(lines) - 1
+        buffer.lines = lines
+    else:
+        buffer.lines.insert(buffer.current_lines_index + 1, lines)
+        buffer.current_line_index += len(lines)
 
 def read(buffer, filename):
     with open(filename, "r") as f:
-	    file_contents = f.readlines()
-        
-        #buffer.currentine_index
-
+	    file_contents = f.read()
+        buffer.lines = file_contents
 
 def write(buffer):
     pass
